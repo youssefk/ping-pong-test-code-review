602ec0868dfca44397082a8b64752887ace82778
Youssef Khalifa <youssefkhalifa@live.com>
2015-10-16 03:43:49 -0700

    Check if anything was put into prompt, tell user if nothing was entered.

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,30 @@ $(document).ready(function() {
 
   var userInput = prompt("Please enter any positive number to see a Ping Pong list up to that number, starting from 1.");
 
-  for (var numCount = 1; numCount <= userInput; numCount += 1) {
+  if (userInput) {
 
-    if (numCount % 3 === 0) {
+    for (var numCount = 1; numCount <= userInput; numCount += 1) {
 
-      if (numCount % 5 === 0) {
+      if (numCount % 3 === 0) {
 
-        $("#list").append("<li>Ping-Pong</li>");
+        if (numCount % 5 === 0) {
+
+          $("#list").append("<li>Ping-Pong</li>");
+
+        } else $("#list").append("<li>Ping</li>");
+
+      } else if (numCount % 5 === 0) {
+
+        $("#list").append("<li>Pong</li>");
+
+      } else {
+
+        $("#list").append("<li>" + numCount + "</li>");
+
       }
 
-      else $("#list").append("<li>Ping</li>");
-
-    } else if (numCount % 5 === 0) {
-
-      $("#list").append("<li>Pong</li>");
-
-    } else {
-
-      $("#list").append("<li>" + numCount + "</li>");
-
     }
-    
-  }
+
+  } else $(".container").append("<h2>You didn't input anything! Reload the page and try again.</h2>");
 
 });
