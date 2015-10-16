ba424d1f183f7dcfedec8012df6be17abfb1561f
Youssef Khalifa <youssefkhalifa@live.com>
2015-10-16 02:35:05 -0700

    Check to see if current number is perfectly divisible by 3 or 5 with 0 remainder

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   for (var numCount = 1; numCount <= 35; numCount += 1) {
 
-    if (numCount % 3) {
+    if (numCount % 3 === 0) {
 
       $("#pingponglist").append("<li>" + "Ping" + "</li>");
 
-    } else if (numCount % 5) {
+    } else if (numCount % 5 === 0) {
 
       $("#pingponglist").append("<li>" + "Pong" + "</li>");
 
